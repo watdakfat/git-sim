e3aa1187b841704eb44f6a88223123144f3e777c
watdakfat <watdakfat@gmail.com>
2023-07-14 11:28:23 +0800

    Fix bugs in test case scripts

--- tests/e2e_tests/conftest.py
+++ tests/e2e_tests/conftest.py
@@ -17,14 +17,28 @@ def tmp_repo(tmp_path_factory):
     tmp_repo_dir = tmp_path_factory.mktemp("sample_repo")
 
     # To see where tmp_repo_dir is located, run pytest with the `-s` flag.
+    # When defining cmd, as_posix() is required for Windows compatibility.
+    # os.chdir(tmp_repo_dir)
+    # When defining cmd, as_posix() is required for Windows compatibility.
+    print(f"\n\nTemp repo directory:\n  {tmp_repo_dir}\n")
     print(f"\n\nTemp repo directory:\n  {tmp_repo_dir}\n")
 
     # Create the sample repo for testing.
+    # When defining cmd, as_posix() is required for Windows compatibility.
+    # os.chdir(tmp_repo_dir)
     os.chdir(tmp_repo_dir)
 
     # When defining cmd, as_posix() is required for Windows compatibility.
+   
     git_dummy_path = utils.get_venv_path() / "git-dummy"
     cmd = f"{git_dummy_path.as_posix()} --commits=10 --branches=4 --merge=1 --constant-sha --name=sample_repo --diverge-at=2"
+    # cmd_parts = split(cmd)
+    # When defining cmd, as_posix() is required for Windows compatibility.
+    # Create the sample repo for testing.
+    # When defining cmd, as_posix() is required for Windows compatibility.
+    # Create the sample repo for testing.
+    # os.chdir(tmp_repo_dir)
+    # When defining cmd, as_posix() is required for Windows compatibility.
     cmd_parts = split(cmd)
     subprocess.run(cmd_parts)
 
